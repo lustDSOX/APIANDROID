3ebeebed44518a9a25d37a3199a02727f14c85e3
ермолаевас <@105-02>
2022-11-01 09:21:44 +0300

    [eq

---
+++ app/src/main/java/com/example/apiwork/MainActivity.java
@@ -0,0 +1,249 @@
+package com.example.apiwork;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private AnimalAdapter pAdapter;
+    private List<Animal> animalList = new ArrayList<>();
+
+    ListView item_list;
+    Intent add_activity;
+    Intent item_activity;
+    EditText txt_search;
+    AnimalAdapter adapter;
+    Spinner spinner;
+    ArrayList<Animal> animalList_s;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*
+        Для того чтобы заполнить ListView  нам необходимо создать адптер. Адаптер используется для связи данных (массивы, базы данных)
+        со списком (ListView)
+        */
+        ListView ivProducts = findViewById(R.id.item_list);//Находим лист в который будем класть наши объекты
+        pAdapter = new AnimalAdapter(MainActivity.this, (ArrayList<Animal>) animalList); //Создаем объект нашего адаптера
+        ivProducts.setAdapter(pAdapter); //Cвязывает подготовленный список с адаптером
+
+        new GetProducts().execute(); //Подключение к нашей API в отдельном потоке
+
+        String[] sort_name = { "По кол-ву лет", "По весу", "По имени", "По виду"};
+        spinner= findViewById(R.id.spinner);
+        ArrayAdapter<String> spinner_adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, sort_name);
+        spinner_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinner_adapter);
+        item_list = findViewById(R.id.item_list);
+        txt_search = findViewById(R.id.txtsearch);
+        add_activity = new Intent(MainActivity.this,AddActivity.class);
+        item_activity = new Intent(MainActivity.this,ItemActivity.class);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+            {
+                switch (position) {
+                    case 0:
+                        Collections.sort(animalList, new Comparator<Animal>() {
+                            @Override
+                            public int compare(Animal o1, Animal o2) {
+                                return o1.GetAge()-o2.GetAge();
+                            }
+                        });
+                        adapter = new AnimalAdapter(MainActivity.this, (ArrayList<Animal>) animalList);
+                        item_list.setAdapter(adapter);
+
+                        break;
+                    case 1:
+                        Collections.sort(animalList, new Comparator<Animal>() {
+                            @Override
+                            public int compare(Animal o1, Animal o2) {
+                                return Float.compare(o1.GetWeight(), o2.GetWeight());
+                            }
+                        });
+                        adapter = new AnimalAdapter(MainActivity.this, (ArrayList<Animal>) animalList);
+                        item_list.setAdapter(adapter);
+                        break;
+                    case 2:
+                        Collections.sort(animalList, new Comparator<Animal>() {
+                            @Override
+                            public int compare(Animal o1, Animal o2) {
+                                return o1.name.compareTo(o2.name);
+                            }
+                        });
+                        adapter = new AnimalAdapter(MainActivity.this, (ArrayList<Animal>) animalList);
+                        item_list.setAdapter(adapter);
+                        break;
+                    case 3:
+                        Collections.sort(animalList, new Comparator<Animal>() {
+                            @Override
+                            public int compare(Animal o1, Animal o2) {
+                                return o1.kind.compareTo(o2.kind);
+                            }
+                        });
+                        adapter = new AnimalAdapter(MainActivity.this, (ArrayList<Animal>) animalList);
+                        item_list.setAdapter(adapter);
+                        break;
+                }
+            }
+            public void onNothingSelected(AdapterView<?> parent)
+            {
+
+            }
+
+        });
+
+        item_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Animal item = (Animal) item_list.getItemAtPosition(i);
+                item_activity.putExtra("name",item.name);
+                item_activity.putExtra("kind",item.kind);
+                item_activity.putExtra("age",item.age);
+                item_activity.putExtra("weight",item.weight);
+                item_activity.putExtra("id",item.id);
+                if(item.image != null) {
+                    item_activity.putExtra("image",encodeImage(item.image));
+                }
+                startActivity(item_activity);
+            }
+        });
+
+        txt_search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int i, int i1, int i2) {
+                if(s.toString().equals("")){
+                    // reset listview
+                    adapter = new AnimalAdapter(MainActivity.this,animalList_s);
+                    item_list.setAdapter(adapter);
+                } else {
+                    for(Animal item:animalList_s){
+                        if(!item.name.contains(s.toString())){
+                            animalList.remove(item);
+                        }
+                    }
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+    }
+    private String encodeImage(Bitmap bitmap) {
+        int prevW = 150;
+        int prevH = bitmap.getHeight() * prevW / bitmap.getWidth();
+        Bitmap b = Bitmap.createScaledBitmap(bitmap, prevW, prevH, false);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        b.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream);
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            return Base64.getEncoder().encodeToString(bytes);
+        }
+        return "";
+    }
+    private class GetProducts extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            try {
+                URL url = new URL("https://ngknn.ru:5101/NGKNN/ермолаевас/api/animals");//Строка подключения к нашей API
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection(); //вызываем нашу API
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                /*
+                BufferedReader успрощает чтение текста из потока символов
+                InputStreamReader преводит поток байтов в поток символов
+                connection.getInputStream() получает поток байтов
+                */
+                StringBuilder result = new StringBuilder();
+                String line = "";
+
+                while ((line = reader.readLine()) != null) {
+                    result.append(line);//кладет строковое значение в потоке
+                }
+                return result.toString();
+
+            } catch (Exception exception) {
+                return null;
+            }
+        }
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try
+            {
+                JSONArray tempArray = new JSONArray(s);//преоброзование строки в json массив
+                for (int i = 0;i<tempArray.length();i++)
+                {
+
+                    JSONObject productJson = tempArray.getJSONObject(i);//Преобразование json объекта в нашу структуру
+                    Animal tempProduct = new Animal(
+                            productJson.getString("Name"),
+                            productJson.getString("Kind"),
+                            productJson.getString("Age"),
+                            productJson.getString("Weight"),
+                            productJson.getString("Id"),
+                            getImageBitmap( productJson.getString("Image"))
+                    );
+                    animalList.add(tempProduct);
+                    pAdapter.notifyDataSetInvalidated();
+                }
+            } catch (Exception ignored) {
+
+
+            }
+        }
+        private Bitmap getImageBitmap(String encodedImg) {
+            if (encodedImg != null) {
+                byte[] bytes = new byte[0];
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                    bytes = Base64.getDecoder().decode(encodedImg);
+                }
+                return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+            }
+            return BitmapFactory.decodeResource(getResources(),
+                    R.drawable.icon);
+        }
+        public void AddAnimal(View v){
+            startActivity(add_activity);
+        }
+
+    }
+}
